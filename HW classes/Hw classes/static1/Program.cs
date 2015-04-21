83941e2ca3b74a35d225091dac9f541145cf36dd
chokanaut <chokanaut@gmail.com>
2015-04-22 00:01:35 +0300

    add static classes hw

---
+++ HW classes/Hw classes/static1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static1
+{
+    class Book
+    {
+        public class Notes
+        {
+            private string[] texts = new string[20];
+
+            public void AddNote(int i, string text)
+            {
+                texts[i] = text;
+            }
+
+        }
+
+        public Notes notes = new Notes();
+
+        public Book()
+        {
+            WriteNote(0, "first note");
+        }
+
+        public void WriteNote(int i, string text)
+        {
+            notes.AddNote(i, text);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.WriteNote(1, "second note");
+        }
+    }
+}
---
+++ HW classes/Hw classes/static2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static2
+{
+    static class ArrayExtensions
+    {
+        public static void Sort(this int[] array)
+        {
+            int length = array.Length;
+
+            int temp = array[0];
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+
+                        array[i] = array[j];
+
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            arr[0] = 2;
+            arr[1] = 1;
+            arr[2] = 0;
+            arr[3] = 9;
+            arr[4] = 3;
+
+            ArrayExtensions.Sort(arr);
+
+            arr.Sort();
+        }
+    }
+}
+
